b372350d7417a1051854f691218004221fc1cfcc
santi <sdvendramini@gmail.com>
2017-10-18 20:05:24 -0300

    promt con user, host y path

--- parteA.c
+++ parteA.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pwd.h>
 
 
 
 int main(int argc, char *argv[])
 {
-
-	char * user = getlogin();
-	char * host[20];
+	//char* user=getpwuid(getuid());
+	//int i = getlogin_r(user, 20);
+	char *user;
+	struct passwd *pass;
+	pass = getpwuid(getuid());
+	user = pass->pw_name;
+	
+	char host[20];
 	gethostname(host, 20);
-	char * command[50];
+	char command[50];
 	while(1){
 
-		printf("%s@%s:%s$ ", user, host,getcwd(NULL,50));
-		gets (command);
-
+		printf("%s@%s:%s$ ", user, host, getcwd(NULL,50));
+		fgets(command,49,stdin);
 	}
 
 	return 0;
