b52ad2fcc587b00b2d9731be079cb6d1cc8b9afd
santi <sdvendramini@gmail.com>
2017-10-25 20:26:10 -0300

    funcionalidad exit

--- parteA.c
+++ parteA.c
@@ -3,6 +3,10 @@
 #include <string.h>
 #include <unistd.h>
 #include <pwd.h>
+#include <stdbool.h>
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
 
 
 
@@ -18,12 +22,22 @@ int main(int argc, char *argv[])
 	char host[20];
 	gethostname(host, 20);
 	char command[50];
+	
+	char command_exit[]="exit\n";
+
 	while(1){
 
-		printf("%s@%s:%s$ ", user, host, getcwd(NULL,50));
+		printf( ANSI_COLOR_RED "%s@%s:%s$ "ANSI_COLOR_RESET , user, host, getcwd(NULL,50) );
 		fgets(command,49,stdin);
-	}
+			//printf("%s",command);
+			//printf("%s",command_exit);
 
+		if (strcmp(command,command_exit)==0)
+		{
+			//printf("%s",command);
+			return 0;
+		}
+	}
 	return 0;
 
 }
